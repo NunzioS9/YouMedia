da56258c35217ef77bb6fbf9f46b23889a03d510
Nunzio Saitta <uni379734@studium.unict.it>
2022-01-09 17:37:15 +0100

    Initial Commit

---
+++ src/main/java/dsbd2122/youmedia/YouMediaApplication.java
@@ -0,0 +1,13 @@
+package dsbd2122.youmedia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YouMediaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(YouMediaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/dsbd2122/youmedia/YouMediaApplicationTests.java
@@ -0,0 +1,13 @@
+package dsbd2122.youmedia;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YouMediaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
