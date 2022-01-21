ac8a16132b7ddf6f4fef97632304597a43890b34
Nunzio Saitta <uni379734@studium.unict.it>
2022-01-21 01:03:40 +0100

    ApiGateway

---
+++ ApiGateway/src/main/java/dsbd2122/apigateway/GatewayRoutes.java
@@ -0,0 +1,28 @@
+package dsbd2122.apigateway;
+
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class GatewayRoutes {
+    @Bean
+    public RouteLocator routeLocator(RouteLocatorBuilder builder) {
+       return builder.routes()
+                .route( r ->
+                        r.path("/user/**")
+                                .uri("http://appcustomer:9091/")
+                )
+                 .route( r ->
+                        r.path("/payment/**")
+                                .uri("http://apppayment:9092/")
+                )
+                 .route( r ->
+                        r.path("/content/**")
+                                .uri("http://appresource:9090/")
+                )
+                .build();
+    }
+}
+
